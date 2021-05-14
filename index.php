9ba537227e47b20df813a50b7b2a2351b6b63a97
Aimgod2005 <rokadebhushan2005@gmail.com>
2021-05-14 06:46:51 -0700

    BHushan Index

---
+++ index.php
@@ -0,0 +1,362 @@
+﻿<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link rel="stylesheet" href="assets\css\bootstrap.min.css" />
+      <link rel="stylesheet" href="assets\css\animate.min.css" />
+      <link rel="stylesheet" href="assets\css\meanmenu.css" />
+      <link rel="stylesheet" href="assets\css\boxicons.min.css" />
+      <link rel="stylesheet" href="assets\css\flaticon.css" />
+      <link rel="stylesheet" href="assets\css\odometer.min.css" />
+      <link rel="stylesheet" href="assets\css\slick.min.css" />
+      <link rel="stylesheet" href="assets\css\nice-select.min.css" />
+      <link rel="stylesheet" href="assets\css\owl.carousel.min.css" />
+      <link rel="stylesheet" href="assets\css\owl.theme.default.min.css" />
+      <link rel="stylesheet" href="assets\css\magnific-popup.min.css" />
+      <link rel="stylesheet" href="assets\css\fancybox.min.css" />
+      <link rel="stylesheet" href="assets\css\style.css" />
+      <link rel="stylesheet" href="assets\css\responsive.css" />
+      <title>Psylo - Life Coach & Psychologist HTML Template</title>
+      <link rel="icon" type="image/png" href="assets\images\sign.sagar.png" />
+   </head>
+   <body>
+      <div class="preloader-area">
+         <div class="spinner">
+            <div class="inner">
+               <div class="disc"></div>
+               <div class="disc"></div>
+               <div class="disc"></div>
+            </div>
+         </div>
+      </div>
+      <?php include 'header.php';?>
+      <div class="search-overlay">
+         <div class="d-table">
+            <div class="d-table-cell">
+               <div class="search-overlay-layer"></div>
+               <div class="search-overlay-layer"></div>
+               <div class="search-overlay-layer"></div>
+               <div class="search-overlay-close">
+                  <span class="search-overlay-close-line"></span>
+                  <span class="search-overlay-close-line"></span>
+               </div>
+               <div class="search-overlay-form">
+                  <form>
+                     <input type="text" class="input-search" placeholder="Search here..." />
+                     <button type="submit"><i class="flaticon-search"></i></button>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="main-banner-area">
+         <div class="main-banner-item">
+            <div class="container">
+               <div class="main-banner-content">
+                  <h1>An Extraordinary Life Starts With <span>Selfcare</span></h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam.</p>
+                  <div class="banner-btn">
+                     <a href="online-booking.html" class="default-btn">Book Online Session <i class="flaticon-pointer"></i></a>
+                     <a href="services.html" class="optional-btn">Our Services <i class="flaticon-repairing-service"></i></a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <section class="features-area pb-70">
+         <div class="container">
+            <div class="row justify-content-center">
+               <div class="col-lg-3 col-sm-6">
+                  <div class="single-features">
+                     <img src="assets/images/2.jpg" style="width:95%">
+                     <h4 class="text-center">Courses</h4>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="single-features">
+                     <img src="assets/images/videos.jpg" style="width:95%">
+                     <h4 class="text-center">Videos</h4>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="single-features">
+                     <img src="assets/images/blogs.jpg" style="width:95%">
+                     <h4 class="text-center">Blogs</h4>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="single-features">
+                     <img src="assets/images/ebooks.jpg" style="width:95%">
+                     <h4 class="text-center">Ebooks</h4>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="about-area pb-100">
+         <div class="container">
+            <div class="row align-items-center">
+               <div class="col-lg-6">
+                  <div class="about-main-image">
+                     <img src="assets\images\samlani.png" alt="image" />
+                     <div class="about-shape">
+                        <div class="shape-2">
+                           <img src="assets\images\about\shape-2.png" alt="image" />
+                        </div>
+                        <div class="shape-5">
+                           <img src="assets\images\about\shape-5.png" alt="image" />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <div class="about-main-content">
+                     <div class="about-content-image">
+                        <img src="assets\images\about\about-2.jpg" alt="image" />
+                        <h4 class="sub-title">Sagar Amlani</h4>
+                        <a href="https://www.youtube.com/watch?v=ODfy2YIKS1M" class="video-btn popup-youtube">
+                        <i class="flaticon-play"></i>
+                        </a>
+                     </div>
+                     <h1>I am Sagar Amlani</h1>
+                     <h2>Professional Speaker</h2>
+                     <p>Productivity = Efficiency X Effectiveness​
+                        <br>
+                        I work with organisation to establish Autonomous work culture, improving productivity leading to sustainable growth.​
+                     </p>
+                     <div class="row">
+                        <div class="col-lg-6 col-sm-6">
+                           <div class="about-information">
+                              <i class="flaticon-trophy"></i>
+                              <h5>12</h5>
+                              <span>Happy Clients</span>
+                           </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                           <div class="about-information">
+                              <i class="flaticon-customer"></i>
+                              <h5>45</h5>
+                              <span>Talks Given</span>
+                           </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                           <div class="about-information">
+                              <i class="flaticon-customer"></i>
+                              <h5>2700</h5>
+                              <span>Live Subscribers</span>
+                           </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-6">
+                           <div class="about-information">
+                              <i class="flaticon-customer"></i>
+                              <h5>3246</h5>
+                              <span>Lives Inspired</span>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="about-btn" style="margin:5% 3%">
+                        <a href="about.html" class="default-btn" style="margin-right: 3%">Learning Resourse <i class="flaticon-graduation9"></i></a>
+                        <a href="about.html" class="default-btn">About Sagar <i class="flaticon-user"></i></a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="about-main-shape">
+            <img src="assets\images\about\line-shape.png" alt="image" />
+         </div>
+      </section>
+      <div class="partner-area ptb-100">
+         <div class="container">
+            <div class="partner-slides owl-carousel owl-theme">
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner1.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner2.png" alt="image" /></a>
+               </div>
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner3.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner4.png" alt="image" /></a>
+               </div>
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner5.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner6.png" alt="image" /></a>
+               </div>
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner7.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner8.png" alt="image" /></a>
+               </div>
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner9.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner10.png" alt="image" /></a>
+               </div>
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner11.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner12.png" alt="image" /></a>
+               </div>
+               <div class="single-partner">
+                  <a href="about.html"><img src="assets\images\partner13.png" alt="image" /></a>
+               </div>
+               <div class="single-partner style-two">
+                  <a href="about.html"><img src="assets\images\partner14.png" alt="image" /></a>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+      <div class="reviews-main-shape">
+         <img src="assets\images\reviews\shape-1.png" alt="image" />
+      </div>
+
+      <section class="coaches-details-area ptb-100">
+         <div class="container">
+            <div class="row align-items-center">
+               <div class="col-lg-4 col-md-4">
+                  <div class="coaches-details-image">
+                     <img src="assets\images\samlani.png" alt="image">
+                  </div>
+               </div>
+               <div class="col-lg-8 col-md-8">
+                  <div class="coaches-details-content">
+                     <h3>About Sagar</h3>
+                     <p>Sagar Amlani is a global speaker, productivity mindset explorer and an author. Though he has years of professional experience on the productivity mindset, what sets him apart is his initial life struggles. These struggles made him learn the value of time, money, and education early in life. Also, these were the same challenges, that became the foundation of Sagar’s innate personality.His respect and gratitude towards his initial life challenges make him a unique person, even after living in the slums. He firmly believes that the difficulties of his initial life are instrumental in his growth and success. The effectiveness and efficiency that he needed to demonstrate and practice during that period of his life make him a natural expert in Productivity mindset.His poverty taught him to plan his resources and utilize them effectively and efficiently from early life.</p>
+                 </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <section class="about-area bg-ffffff ptb-100">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-6">
+            <div class="about-main-content">
+               <h3>Strong foundation</h3>
+               <p>Sagar was born in a humble Gujrati family and grew up in the famous slums of Mumbai. His father was a very small cloth trader, and his mother was a private tuition teacher to make the ends meet. Scarcity of resources was usual during those days; it taught him the most critical lesson to identify and manage available resources well. Many things demanded precision in planning and execution, from quarterly roof cleaning to the monthly drinking water tank cleaning. Or else, hazardous life-threatening accidents could happen. These experiences acted as early foundations for him to be christened as the Productivity Mindset Explorer of today.</p>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-4">
+            <div class="about-main-image">
+               <img src="assets\images\samlani.png" alt="image">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+        <section class="blog-area pt-100 pb-70">
+         <div class="container">
+            <div class="section-title">
+               <h2>Dive Into Our Blog</h2>
+               <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="row justify-content-center">
+               <div class="col-lg-4 col-md-6">
+                  <div class="single-blog">
+                     <div class="blog-image">
+                        <a href="blog-details.html"><img src="assets\images\blog\blog-1.jpg" alt="image" /></a>
+                        <div class="tag">13th <span>Jan</span></div>
+                     </div>
+                     <div class="blog-content">
+                        <h3>
+                           <a href="blog-details.html">How To Take A Short Break From The Social Media</a>
+                        </h3>
+                        <div class="blog-btn">
+                           <a href="blog-details.html" class="default-btn">Read More <i class="flaticon-plus"></i></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6">
+                  <div class="single-blog">
+                     <div class="blog-image">
+                        <a href="blog-details.html"><img src="assets\images\blog\blog-2.jpg" alt="image" /></a>
+                        <div class="tag">14th <span>Jan</span></div>
+                     </div>
+                     <div class="blog-content">
+                        <h3>
+                           <a href="blog-details.html">15 Ways To Be Happy Alone and Live A Full Life</a>
+                        </h3>
+                        <div class="blog-btn">
+                           <a href="blog-details.html" class="default-btn">Read More <i class="flaticon-plus"></i></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6">
+                  <div class="single-blog">
+                     <div class="blog-image">
+                        <a href="blog-details.html"><img src="assets\images\blog\blog-3.jpg" alt="image" /></a>
+                        <div class="tag">14th <span>Jan</span></div>
+                     </div>
+                     <div class="blog-content">
+                        <h3>
+                           <a href="blog-details.html">How Does A Psychologist Help To Maintain A Good Mental Health</a>
+                        </h3>
+                        <div class="blog-btn">
+                           <a href="blog-details.html" class="default-btn">Read More <i class="flaticon-plus"></i></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <div class="subscribe-area">
+         <div class="container">
+            <div class="subscribe-inner-box">
+               <div class="subscribe-content">
+                  <h2>Subscribe Our Newsletter</h2>
+                  <p>Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam voluptua.</p>
+                  <form class="newsletter-form" data-bs-toggle="validator">
+                     <div class="row">
+                        <div class="col-lg-8 col-md-6">
+                           <input type="email" class="input-newsletter" placeholder="Enter your email" name="EMAIL" required="" autocomplete="off" />
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                           <button type="submit">Subscribe Now <i class="flaticon-paper-plane"></i></button>
+                           <div id="validator-newsletter" class="form-result"></div>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <?php include 'footer.php';?>
+      <div class="go-top">
+         <i class="flaticon-up-arrow"></i>
+      </div>
+      <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+      <script src="assets\js\jquery.min.js"></script>
+      <script src="assets\js\bootstrap.bundle.min.js"></script>
+      <script src="assets\js\jquery.meanmenu.js"></script>
+      <script src="assets\js\owl.carousel.min.js"></script>
+      <script src="assets\js\jquery.appear.js"></script>
+      <script src="assets\js\odometer.min.js"></script>
+      <script src="assets\js\slick.min.js"></script>
+      <script src="assets\js\nice-select.min.js"></script>
+      <script src="assets\js\jquery.magnific-popup.min.js"></script>
+      <script src="assets\js\fancybox.min.js"></script>
+      <script src="assets\js\jquery.ajaxchimp.min.js"></script>
+      <script src="assets\js\form-validator.min.js"></script>
+      <script src="assets\js\contact-form-script.js"></script>
+      <script src="assets\js\wow.min.js"></script>
+      <script src="assets\js\main.js"></script>
+   </body>
+</html>
